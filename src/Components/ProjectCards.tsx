d32ba6e95f5a7db7b5c5167a8d040ba9d2a0029a
Battashi <battashi.dev@gmail.com>
2026-07-20 09:19:44 +0400

    fixed expensive tracker live link

--- src/Components/ProjectCards.tsx
+++ src/Components/ProjectCards.tsx
@@ -14,7 +14,7 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { PROJECTS } from "./Projects";
-import logo from "../assets/battashi-logo.svg";
+import logo from "../assets/ExpenseTracker.png";
 
 const ProjectCards = () => {
   return (
@@ -28,46 +28,48 @@ const ProjectCards = () => {
           borderColor="whiteAlpha.100"
           boxShadow="lg"
           _hover={{ transform: "translateY(-4px)", transition: "0.2s" }}
-          direction={{base: "column", md: "row"}}
+          direction={{ base: "column", md: "row" }}
         >
-          <Image
+          <HStack>
+             <Image
             src={logo}
             alt="title"
             objectFit="cover"
             w={{ base: "100%", md: "40%" }}
-            aspectRatio={{ base: 16/9, md:4/3}}
-            
-          />
-          <Box>
-          <CardHeader>
-            <Link href={githubHref} isExternal>
-              <Heading size="lg">{title}</Heading>
-            </Link>
-          </CardHeader>
-          <CardBody>
-            <Text>{description}</Text>
-            <HStack spacing={2} mt={2} flexWrap="wrap">
-              {tags.map(({ label, colorScheme }) => (
-                <Tag key={label} colorScheme={colorScheme}>
-                  {label}
-                </Tag>
-              ))}
-            </HStack>
-          </CardBody>
-          <CardFooter>
-            <Link href={demoHref}>
-              <Button
-                fontWeight="bold"
-                size="sm"
-                color="white"
-                bg="brand.accent"
-                _hover={{ opacity: 0.85 }}
-              >
-                View Live Demo
-              </Button>
-            </Link>
-          </CardFooter>
-          </Box>
+            aspectRatio={{ base: 16 / 9, md: 4 / 3 }}
+            />
+  
+            <Box>
+              <CardHeader>
+                <Link href={githubHref} isExternal>
+                  <Heading size="lg">{title}</Heading>
+                </Link>
+              </CardHeader>
+              <CardBody>
+                <Text>{description}</Text>
+                <HStack spacing={2} mt={2} flexWrap="wrap">
+                  {tags.map(({ label, colorScheme }) => (
+                    <Tag key={label} colorScheme={colorScheme}>
+                      {label}
+                    </Tag>
+                  ))}
+                </HStack>
+              </CardBody>
+              <CardFooter>
+                <Link href={demoHref}>
+                  <Button
+                    fontWeight="bold"
+                    size="sm"
+                    color="white"
+                    bg="brand.accent"
+                    _hover={{ opacity: 0.85 }}
+                  >
+                    View Live Demo
+                  </Button>
+                </Link>
+              </CardFooter>
+            </Box>
+          </HStack>
         </Card>
       ))}
     </SimpleGrid>
--- src/Components/Projects.ts
+++ src/Components/Projects.ts
@@ -2,17 +2,17 @@ export const PROJECTS = [
   {
     title: "Expensive Tracker",
     githubHref: "https://github.com/Battashi-Dev/expensive-tracker",
-    demoHref: "https://expensive-tracker-vert.vercel.app/",
+    demoHref: "https://expensive-tracker-wheat.vercel.app/",
     description:
       "A React + TypeScript Expense Tracker with localStorage persistence, Zod validation, and React Hook Form. built with custom hooks for state management and Chakra UI for the interface",
-tags: [
-    { label: "React", colorScheme: "blue"},
-    { label: "TypeScript", colorScheme: "blue"},
-    { label: "Zod", colorScheme: "purple"},
-    { label: "React hook Form", colorScheme: "pink"},
-    { label: "Chakra UI", colorScheme: "gray"},
-] 
-    },
-    
+    tags: [
+      { label: "React", colorScheme: "blue" },
+      { label: "TypeScript", colorScheme: "blue" },
+      { label: "Zod", colorScheme: "purple" },
+      { label: "React hook Form", colorScheme: "pink" },
+      { label: "Chakra UI", colorScheme: "gray" },
+    ],
+  },
+
   // next project comes here
 ] as const;
